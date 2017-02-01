b828c0d0841bc49b7d253d879b00b470cb0df690
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-01 10:46:08 -0800

    not done yet..just trying to figure this out

--- Turtle.py
+++ Turtle.py
@@ -1,4 +1,14 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
+import turtle
+turtle = turtle.Turtle()
+l= 90
+n = 90
+def square(l,n):
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.right(90)
+    turtle.left(90)
+print(l,n)
 
 
 
