bb20d16e0ba04561bcffb96b2ee935154ffa6f02
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-01 10:28:41 -0800

    encryption

--- Encrypt.py
+++ Encrypt.py
@@ -3,7 +3,29 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
+"""
+def Ciphertext(pie):
+    Ciphertext = ""
+    for i in pie:
+        if i =='a':
+            Ciphertext = Ciphertext + '%'
+        elif i == 'e':
+            Ciphertext = Ciphertext + '^'
+        elif i == 'i':
+            Ciphertext = Ciphertext + '$'
+        elif i == 'o':
+            Ciphertext = Ciphertext + '@'
+        elif i == '*':
+            Ciphertext = Ciphertext + '!'
+        else:
+            Ciphertext = Ciphertext + i
+    return Ciphertext
+print(Ciphertext('onomonopia'))
+
+
+
+
+
 
 
 
