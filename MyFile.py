cf321e90c72cf06f652894d1686e8e058398bf15
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-01 11:27:46 -0800

    drawrectangle

--- MyFile.py
+++ MyFile.py
@@ -1,3 +1,18 @@
 def bob(n):
     n = n + 10
     return(n)
+
+import turtle
+turtle = turtle.Turtle()
+Anyturtle = 100
+l = 90
+w = 150
+def DrawRectangle(Anyturtle,l,w):
+    turtle.forward(Anyturtle)
+    turtle.left(l)
+    turtle.forward(w)
+    turtle.left(l)
+    turtle.forward(Anyturtle)
+    turtle.left(l)
+    turtle.forward(w)
+DrawRectangle(Anyturtle,l,w)
