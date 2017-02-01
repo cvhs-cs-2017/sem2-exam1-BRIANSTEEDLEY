a5bacc73d3fe24dae4e75ba998e51e2fc2a3509b
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-01 11:12:09 -0800

    rectangle

--- Turtle.py
+++ Turtle.py
@@ -1,14 +1,4 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
-import turtle
-turtle = turtle.Turtle()
-l= 90
-n = 90
-def square(l,n):
-    turtle.forward(90)
-    turtle.left(90)
-    turtle.right(90)
-    turtle.left(90)
-print(l,n)
 
 
 
@@ -16,3 +6,17 @@ print(l,n)
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
+import turtle
+turtle = turtle.Turtle()
+Anyturtle = 100
+l = 90
+w = 150
+def DrawRectangle(Anyturtle,l,w):
+    turtle.forward(Anyturtle)
+    turtle.left(l)
+    turtle.forward(w)
+    turtle.left(l)
+    turtle.forward(Anyturtle)
+    turtle.left(l)
+    turtle.forward(w)
+DrawRectangle(Anyturtle,l,w)
