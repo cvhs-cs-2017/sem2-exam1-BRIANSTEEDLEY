2ba6300089621946f9b713c8841b133b5beeaca2
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-01 11:32:21 -0800

    steve

--- MyFile.py
+++ MyFile.py
@@ -2,17 +2,31 @@ def bob(n):
     n = n + 10
     return(n)
 
+m = int(input('enter a number'))
 import turtle
-turtle = turtle.Turtle()
+steve = turtle.Turtle()
+for i in range(200):
+        steve.forward(10)
+        steve.left(3.6)
+
+import turtle
+PB = turtle.Turtle()
 Anyturtle = 100
 l = 90
 w = 150
 def DrawRectangle(Anyturtle,l,w):
-    turtle.forward(Anyturtle)
-    turtle.left(l)
-    turtle.forward(w)
-    turtle.left(l)
-    turtle.forward(Anyturtle)
-    turtle.left(l)
-    turtle.forward(w)
+    PB.forward(Anyturtle)
+    PB.left(l)
+    PB.forward(w)
+    PB.left(l)
+    PB.forward(Anyturtle)
+    PB.left(l)
+    PB.forward(w)
 DrawRectangle(Anyturtle,l,w)
+
+m = int(input('enter a number'))
+import turtle
+steve = turtle.Turtle()
+for i in range(200):
+    steve.forward(x)
+    steve.left(3.6)
