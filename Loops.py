4308386aeb56549fa385528ae18ab5c76867ba34
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-02-03 10:43:42 -0800

    confused on loops

--- Loops.py
+++ Loops.py
@@ -2,9 +2,25 @@
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
 
+import turtle
+
+turtle = turtle.Turtle()
+turtle.speed(0)
+x = 100
+y = 100
+theta = 100
+def triangle(x,y,theta):
+    for i in range(1):
+        turtle.pu()
+        turtle.goto(-100,100)
+        turtle.pd()
+        turtle.forward(x)
+        turtle.right(y)
+        turtle.forward(theta)
+        turtle.goto(-100,100)
 
 
-
+triangle(x,y,theta)
 
 
 """Write a program that will prompt the user for an input value (n) and double
